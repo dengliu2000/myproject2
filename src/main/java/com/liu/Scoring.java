dff80e5a5a3500fc4ae202cbc83ff547fcd23164
dengliu <dengliu2000@gmail.com>
2022-03-11 14:28:14 +0800

    print names,english,math

--- src/main/java/com/liu/Scoring.java
+++ src/main/java/com/liu/Scoring.java
@@ -2,9 +2,12 @@ package com.liu;
 
 public class Scoring {
     public static void main(String[] args) {
-        Students Jack=new Students();
-        Jack.names="Jack";
+        Students Jack=new Students("Jack");
+        //Jack.names="Jack";
         Jack.english=70;
         Jack.math=85;
+        Students Hank=new Students("Hank",60,30);
+        Jack.print();
+        Hank.print();
     }
 }
--- src/main/java/com/liu/Students.java
+++ src/main/java/com/liu/Students.java
@@ -4,4 +4,15 @@ public class Students {
     String names;
     int english;
     int math;
+    public Students(String names){
+        this.names=names;
+    }
+    public Students(String names,int english,int math){
+        this.names=names;
+        this.english=english;
+        this.math=math;
+    }
+    public void print(){
+        System.out.println(names+"\t"+english+"\t"+math+"\t");
+    }
 }
