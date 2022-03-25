bae3c22f7549c136d16553c2e84ab2f5898e5c5a
dengliu2000 <dengliu2000@gmail.com>
2022-03-25 16:29:17 +0800

    ticket_third

--- src/main/java/com/liu/ticket/Station.java
+++ src/main/java/com/liu/ticket/Station.java
@@ -1,9 +1,13 @@
 package com.liu.ticket;
 
 public class Station {
-    public static final Station TAIPEI_STATION=new Station();
+    public static final Station TAIPEI_STATION=new Station(100,);
     public static final Station TAICHUNG_STATION=new Station();
     public static final Station KAOHSIUNG_STATION=new Station();
     int id;
     String name;
+    public Station(int id,String name){
+        this.id=id;
+        this.name=name;
+    }
 }
