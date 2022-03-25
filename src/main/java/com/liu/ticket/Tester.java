833c0445f0175b39655efcf3dec14a1077ffde22
dengliu2000 <dengliu2000@gmail.com>
2022-03-25 16:05:21 +0800

    ticket_first

---
+++ src/main/java/com/liu/ticket/Tester.java
@@ -0,0 +1,7 @@
+package com.liu.ticket;
+
+public class Tester {
+    public static void main(String[] args) {
+        Ticket ticket=new Ticket(Ticket.TAIPEI_STATION,Ticket.TAICHUNG_STATION);
+    }
+}
