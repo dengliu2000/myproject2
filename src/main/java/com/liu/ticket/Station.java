e8636c6298d66e1e53fb08b3e57d16a20d0c2ce4
dengliu2000 <dengliu2000@gmail.com>
2022-03-25 16:26:22 +0800

    ticket_second

---
+++ src/main/java/com/liu/ticket/Station.java
@@ -0,0 +1,9 @@
+package com.liu.ticket;
+
+public class Station {
+    public static final Station TAIPEI_STATION=new Station();
+    public static final Station TAICHUNG_STATION=new Station();
+    public static final Station KAOHSIUNG_STATION=new Station();
+    int id;
+    String name;
+}
--- src/main/java/com/liu/ticket/Tester.java
+++ src/main/java/com/liu/ticket/Tester.java
@@ -3,5 +3,6 @@ package com.liu.ticket;
 public class Tester {
     public static void main(String[] args) {
         Ticket ticket=new Ticket(Ticket.TAIPEI_STATION,Ticket.TAICHUNG_STATION);
+        System.out.println("Your start station()");
     }
 }
