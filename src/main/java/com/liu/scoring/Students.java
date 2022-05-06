122c55cd4d3346201f2618b94abe22de5d6f2074
DESKTOP-OA6FUQH\nick <dengliu2000@gmail.com>
2022-05-06 15:52:37 +0800

    ticket 2

--- src/main/java/com/liu/scoring/Students.java
+++ src/main/java/com/liu/scoring/Students.java
@@ -1,6 +1,6 @@
 package com.liu.scoring;
 
-public class Students {
+public class Students implements Printable{
     String names;
     int english;
     int math;
@@ -9,15 +9,24 @@ public class Students {
         this.english=english;
         this.math=math;
     }
-    public Students(){
-        this("John Doe",-1,-1);
-    }
-    public void print(){
+
+    @Override
+    public void print() {
         System.out.print(names+"\t"+english+"\t"+math+"\t"+getAverage());
         if (getAverage()<60)
             System.out.print("*");
         System.out.println();
     }
+
+    public Students(){
+        this("John Doe",-1,-1);
+    }
+    /*public void print(){
+        System.out.print(names+"\t"+english+"\t"+math+"\t"+getAverage());
+        if (getAverage()<60)
+            System.out.print("*");
+        System.out.println();
+    }*/
     public int getAverage(){
         return (math+english)/2;
     }
--- src/main/java/com/liu/ticket/Ticket.java
+++ src/main/java/com/liu/ticket/Ticket.java
@@ -3,14 +3,28 @@ package com.liu.ticket;
 public class Ticket {
     public static final int TAIPEI_STATION = 100;
     public static final int TAICHUNG_STATION = 200;
-    public static final int KAOHSIUNG_STATION = 300;
+    public static final int KAOHSIUNG_STATION = 400;
     Station start;
     Station destination;
     int price;
-    public Ticket(Station start,Station destination){
-        this.start=start;
-        this.destination=destination;
-        if (start==Station.TAIPEI_STATION) {
+    public Ticket(Station start,Station destination) {
+        this.start = start;
+        this.destination = destination;
+        int diff = Math.abs(start.id - destination.id);
+        System.out.println("diff:" + diff);
+        switch (diff) {
+            case 100:
+                price = 500;
+                break;
+            case 200:
+                price = 600;
+                break;
+            case 300:
+                price = 1100;
+                break;
+        }
+    }
+        /*if (start==Station.TAIPEI_STATION) {
             if (destination == Station.TAICHUNG_STATION) {
                 price = 600;
             } else {
@@ -32,7 +46,7 @@ public class Ticket {
                         price=900;
                     }
                 }
-            }
+            }*/
             public void print(){
                 System.out.println(start.name+"\t"+destination.name+"\t"+price);
             }
