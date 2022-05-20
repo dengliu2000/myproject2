085b6c1ea81637a10996d5a3a59b781b88f1eaf6
DESKTOP-OA6FUQH\nick <dengliu2000@gmail.com>
2022-05-20 15:48:16 +0800

    some button

--- src/main/java/com/liu/ui/MyWin.java
+++ src/main/java/com/liu/ui/MyWin.java
@@ -1,6 +1,7 @@
 package com.liu.ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MyWin {
     public static void main(String[] args) {
@@ -9,7 +10,15 @@ public class MyWin {
         frame.setLocation(300,200);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         JButton button=new JButton("OK");
+        frame.setLayout(new FlowLayout());
         frame.add(button);
+        frame.add(new JButton("1"));
+        frame.add(new JButton("2"));
+        frame.add(new JButton("3"));
+        frame.add(new JButton("4"));
+        frame.add(new JButton("5"));
+        frame.add(new JButton("6"));
+        frame.add(new JButton("7"));
         frame.setVisible(true);
         System.out.println("END???");
     }
