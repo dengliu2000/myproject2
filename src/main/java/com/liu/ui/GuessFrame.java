f3a3b6c2189b10e98016de920e3b3032a676de30
DESKTOP-OA6FUQH\nick <dengliu2000@gmail.com>
2022-05-27 16:29:39 +0800

    guess secret number version 1

--- src/main/java/com/liu/ui/GuessFrame.java
+++ src/main/java/com/liu/ui/GuessFrame.java
@@ -4,24 +4,38 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class GuessFrame extends JFrame {
     public GuessFrame(){
         super();
+        int secret=new Random().nextInt(10)+1;
+        System.out.println("secret "+secret);
         setSize(600,400);
         setLocation(300,200);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        JButton button=new JButton("hi");
+        JButton button=new JButton("Guess!!!");
         JLabel label=new JLabel("Hey!!!");
+        JTextField number=new JTextField(8);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //System.out.println("Hello");
-                label.setText("Hello");
+                int num= Integer.parseInt(number.getText());
+                System.out.println(num);
+                if (num>secret){
+                    label.setText("smaller");
+                }
+                else if(num<secret){
+                    label.setText("bigger");
+                }
+                else {
+                    label.setText("Bingo, the secret number "+secret);
+                }
             }
         });
 
         setLayout(new FlowLayout());
+        add(number);
         add(button);
         add(label);
         setVisible(true);
