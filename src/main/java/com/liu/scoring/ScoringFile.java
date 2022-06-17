b5f441ecd648024a2c73f299941d907eb5137f96
DESKTOP-OA6FUQH\nick <dengliu2000@gmail.com>
2022-06-17 16:10:53 +0800

    continuous reader

--- src/main/java/com/liu/scoring/ScoringFile.java
+++ src/main/java/com/liu/scoring/ScoringFile.java
@@ -9,9 +9,11 @@ public class ScoringFile {
         try {
             FileReader fileReader=new FileReader("scores.txt");
             int data=fileReader.read();
-            System.out.println(data);
-            data=fileReader.read();
-            System.out.println(data);
+            while (data!=-1){
+                System.out.print((char)data);
+                data=fileReader.read();
+            }
+            System.out.println();
         }catch (FileNotFoundException e){
             System.out.println("Not found file");
         }catch (IOException e){
